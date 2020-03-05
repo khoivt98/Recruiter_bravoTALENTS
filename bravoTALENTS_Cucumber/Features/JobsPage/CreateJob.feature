53f671130e61dd5b8b6a02b2bb0b692a9457fb15
khoivt98 <vothachkhoi98@gmail.com>
2020-03-05 10:44:58 +0700

    Add new test case for create job

--- bravoTALENTS_Cucumber/Features/JobsPage/CreateJob.feature
+++ bravoTALENTS_Cucumber/Features/JobsPage/CreateJob.feature
@@ -17,7 +17,7 @@ Scenario: Create job manually successful
 	And I click on create button
 	Then I create new job successful
 
-Scenario: Create job from template successful
+Scenario: Create job from templates successful
 	Given I navigate to application
 	And I login into bravoTALENTS
 	When I click Create job button
@@ -26,10 +26,62 @@ Scenario: Create job from template successful
 	And I click on create and edit button
 	Then I create new job successful
 
-Scenario: Close the create job panel successful
+Scenario: Create job from previous jobs successful
+	Given I navigate to application
+	And I login into bravoTALENTS
+	When I click Create job button
+	And I enter text into job title field
+	And I click on the job in the previous jobs list
+	And I click on create and edit button
+	Then I create new job successful
+
+Scenario: Close the create job panel successful by using X icon
 	Given I navigate to application
 	And I login into bravoTALENTS
 	When I click Create job button
 	And I click the X icon
 	Then The create job panel should closed
 
+Scenario: Close the create job manually panel successful by using X icon
+	Given I navigate to application
+	And I login into bravoTALENTS
+	When I click Create job button
+	And I enter text into job title field
+	And I click on the Create from scratch button
+	And I click the X icon in the create job manually panel
+	Then The create job manually panel should closed
+
+Scenario: Close the create job and review job panel successful by using X icon
+	Given I navigate to application
+	And I login into bravoTALENTS
+	When I click Create job button
+	And I enter text into job title field
+	And I click on the job in the template list
+	And I click the X icon in the create job and review job panel
+	Then The create job and review job panel should closed
+
+Scenario: Close the create job manually panel successful by using cancel button
+	Given I navigate to application
+	And I login into bravoTALENTS
+	When I click Create job button
+	And I enter text into job title field
+	And I click on the Create from scratch button
+	And I click the cancel button in the create job manually panel
+	Then The create job manually panel should closed
+
+Scenario: Close the create job and review job panel successful by using cancel button
+	Given I navigate to application
+	And I login into bravoTALENTS
+	When I click Create job button
+	And I enter text into job title field
+	And I click on the job in the template list
+	And I click the cancel button in the create job and review job panel
+	Then The create job and review job panel should closed
+
+Scenario: The data in review panel is changed when I click on another job in template list
+	Given I navigate to application
+	And I login into bravoTALENTS
+	When I click Create job button
+	And I enter text into job title field
+	And I click on the job in the template list
+	Then The job title in list and preview panel is equal
--- bravoTALENTS_Cucumber/PageObject/JobsPage/JobPO.cs
+++ bravoTALENTS_Cucumber/PageObject/JobsPage/JobPO.cs
@@ -11,60 +11,68 @@ namespace bravoTALENTS_Cucumber.PageObject
     class JobPO
     {
         [FindsBy(How = How.CssSelector, Using = ".job-action--first [type]")]
-        public IWebElement btnCreateJob { get; set; }
+        public IWebElement createJobButton { get; set; }
 
-        //Create Job from scratch
-        [FindsBy(How = How.CssSelector, Using = ".job-creation-action__from-scratch")]
-        public IWebElement btnCreateFromScratch { get; set; }
-
+        //Create Job panel
         [FindsBy(How = How.CssSelector, Using = ".job-creation-form__label input")]
-        public IWebElement txtJobTitle { get; set; }
+        public IWebElement jobTitleTextBox { get; set; }
 
-        [FindsBy(How = How.CssSelector, Using = ".select2.select2-container.select2-container--default")]
-        public IList<IWebElement> sltJobCategory { get; set; }
+        [FindsBy(How = How.CssSelector, Using = ".job-creation-title__icon.small.sprite-icon.sprite-remove")]
+        public IWebElement closeButtonInCreateJobPanel { get; set; }
 
-        [FindsBy(How = How.CssSelector, Using = ".select2-results__option")]
-        public IList<IWebElement> sltOption { get; set; }
+        [FindsBy(How = How.CssSelector, Using = ".job-creation-action__from-scratch")]
+        public IWebElement createFromScratchButton { get; set; }
 
-        [FindsBy(How = How.CssSelector, Using = ".job-creation-form__location-dropdown")]
-        public IWebElement openSltLocation { get; set; }
-
-        [FindsBy(How = How.CssSelector, Using = ".job-creation-form__location-item")]
-        public IList<IWebElement> sltLocation { get; set; }
-
-        [FindsBy(How = How.CssSelector, Using = ".job-creation-form__submit custom-button:nth-of-type(2) [type]")]
-        public IWebElement btnCreate { get; set; }
-
-
-
-        [FindsBy(How = How.CssSelector, Using = ".job-creation-close-icon.job-creation-list-action__close-button.small.sprite-icon.sprite-remove")]
-        public IWebElement btnClose { get; set; }
-
-        [FindsBy(How = How.CssSelector, Using = ".job-creation-form__submit > custom-button:nth-of-type(1) > .button.button--secondary")]
-        public IWebElement btnCancel { get; set; }
-
-        [FindsBy(How = How.CssSelector, Using = ".job-creation-form__submit custom-button:nth-of-type(2) [type]")]
-        public IWebElement btnSave { get; set; }
-
-        //Create Job from template or previous job
-        [FindsBy(How = How.CssSelector, Using = ".job-creation-with-details__footer custom-button:nth-of-type(2) [type]")]
-        public IWebElement btnCreateAndEdit { get; set; }
-
-        [FindsBy(How = How.CssSelector, Using = ".job-creation-with-details__footer > custom-button:nth-of-type(1) > .button.button--secondary")]
-        public IWebElement btnCancel2 { get; set; }
-
-        [FindsBy(How = How.CssSelector, Using = ".big.job-creation-title__icon.sprite-icon.sprite-remove")]
-        public IWebElement btnClose2 { get; set; }
-
-        [FindsBy(How = How.CssSelector, Using = ".jobs-from-previous__item.text-truncate")]
+        [FindsBy(How = How.CssSelector, Using = "span.jobs-from-previous__job-name")]
         public IList<IWebElement> listItemFromPreviousJobs { get; set; }
 
-        [FindsBy(How = How.CssSelector, Using = ".jobs-from-templates__item.text-truncate")]
+        [FindsBy(How = How.CssSelector, Using = "span.jobs-from-templates__job-name")]
         public IList<IWebElement> listItemFromTemplates { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.jobs-from-previous > span.no-jobs-text")]
+        public IWebElement noPreviousJobsFoundText { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.jobs-from-templates > span.no-jobs-text")]
+        public IWebElement noTemplatesFoundText { get; set; }
+
+        //Create Job from scratch panel    
+        [FindsBy(How = How.CssSelector, Using = ".select2.select2-container.select2-container--default")]
+        public IList<IWebElement> selectDropdown { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".select2-results__option")]
+        public IList<IWebElement> selectDropdownItem { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".job-creation-form__location-dropdown")]
+        public IWebElement locationDropdown { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".job-creation-form__location-item")]
+        public IList<IWebElement> locationItem { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.job-creation-form__submit > custom-button > button.button.button--primary")]
+        public IWebElement createButton { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "i.job-creation-list-action__close-button")]
+        public IWebElement closeButton { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.job-creation-form__submit > custom-button > button.button.button--secondary")]
+        public IWebElement cancelButtonInCreateJobPanel { get; set; }
+
+        //Review job panel
+        [FindsBy(How = How.CssSelector, Using = "div.job-creation-with-details__footer > custom-button > button.button.button--primary")]
+        public IWebElement createAndEditButton { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.job-creation-with-details__footer > custom-button > button.button.button--secondary")]
+        public IWebElement cancelButtonInReivewPanel { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = ".big.job-creation-title__icon.sprite-icon.sprite-remove")]
+        public IWebElement closeButtonInReivewPanel { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.job-creation-with-details__job-title")]
+        public IWebElement jobTitleInReivewPanel { get; set; }
+
 
         //Job Ad
         [FindsBy(How = How.CssSelector, Using = ".--editable.job-header__job-title-display.ng-star-inserted > div > div")]
-        public IWebElement txtJobName { get; set; }
-
+        public IWebElement jobNameTextBox { get; set; }
     }
 }
